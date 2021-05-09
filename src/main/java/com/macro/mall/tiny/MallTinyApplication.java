64b2a6806e8d5a8f3609cc21d6268b20ebbf07e7
chenhj <chenhj@17uhui.com>
2021-05-09 14:27:42 +0800

    新增分支

--- src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MallTinyApplication {
 
     public static void main(String[] args) {
+        System.out.println("启动");
         SpringApplication.run(MallTinyApplication.class, args);
     }
 
